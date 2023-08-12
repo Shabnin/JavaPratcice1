21e483ed0ad6fee5b34342740e77760ba704a872
Shabnin <shabnins@gmail.com>
2023-08-12 18:15:06 -0400

    12 August

---
+++ src/cw_practice/Session1.java
@@ -0,0 +1,37 @@
+package cw_practice;
+
+
+
+public class Session1 { 
+	//
+	/*
+	 traffic
+	 traffic
+	 traffic
+	 traffic
+	 traffic
+	 traffic
+	 traffic
+	 opal
+	 java
+	 java
+	 java
+	 java
+	 application
+	 */
+	/*
+	 * apple
+	 * orange
+	 * palm
+	 * cherry
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Wolrld!");
+		System.out.println("Hello Wolrld!");
+		System.out.println("Hello Wolrld!");
+		System.out.println("Hello Wolrld!");
+	}
+	
+
+}
